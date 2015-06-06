656d54efd3e47939ad342c61892c04df952b6545
devbego <zach.rispoli@gmail.com>
2015-06-06 14:44:25 -0400

    fixed some build paths to be relative

---
+++ src/PuppetRecording.cpp
@@ -0,0 +1,1 @@
+#include "PuppetRecording.h"
---
+++ src/PuppetRecording.h
@@ -0,0 +1,12 @@
+#ifndef __dranimate__PuppetRecording__
+#define __dranimate__PuppetRecording__
+
+#include <iostream>
+
+class PuppetRecording {
+    
+    
+    
+};
+
+#endif
